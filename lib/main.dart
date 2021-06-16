05b5edbe554a9d94f8cc4a6a7f1501b1b2b8a336
aldebarane1 <matriculemat@gmail.com>
2021-06-16 17:12:29 +0200

    forme button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // import 'package:audioplayers/audioplayers.dart';
+// import 'dart:html';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
@@ -10,16 +12,32 @@ void playsound(int soundNumber) {
   player.play('note$soundNumber.wav');
 }
 
-Expanded buildKey({Color color, int soundNumber}) {
+Expanded buildKey({String note, Color color, int soundNumber}) {
   return Expanded(
     child: TextButton(
+      child: Text(note,
+          style: TextStyle(
+              fontSize: 36,
+              letterSpacing: 4,
+              color: Colors.black,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold)),
       style: TextButton.styleFrom(
         primary: Colors.teal,
         backgroundColor: color,
         onSurface: Colors.grey,
+        shadowColor: Colors.black,
+        elevation: 10,
+        side: BorderSide(color: Colors.black, width: 5),
+        shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(60))),
       ),
       onPressed: () {
         playsound(soundNumber);
+        print(note);
+      },
+      onLongPress: () {
+        print('stoooooooooooop');
       },
     ),
   );
@@ -35,13 +53,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
+              buildKey(note: 'Do', color: Colors.red, soundNumber: 1),
+              buildKey(note: 'Ré', color: Colors.orange, soundNumber: 2),
+              buildKey(note: 'Mi', color: Colors.yellow, soundNumber: 3),
+              buildKey(note: 'Fa', color: Colors.green, soundNumber: 4),
+              buildKey(note: 'Sol', color: Colors.teal, soundNumber: 5),
+              buildKey(note: 'La', color: Colors.blue, soundNumber: 6),
+              buildKey(note: 'Si', color: Colors.purple, soundNumber: 7),
             ],
           ),
         ),
